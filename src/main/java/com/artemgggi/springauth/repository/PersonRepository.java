a1b73214483a4e477201cc16224db56793e59a1e
Artem <artem.ivanchikov.99@mail.ru>
2022-06-16 21:30:13 +0600

    added rest crud Person controller

--- src/main/java/com/artemgggi/springauth/repository/PersonRepository.java
+++ src/main/java/com/artemgggi/springauth/repository/PersonRepository.java
@@ -1,4 +1,7 @@
 package com.artemgggi.springauth.repository;
 
-public interface PersonRepository {
+import com.artemgggi.springauth.domain.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Integer> {
 }
